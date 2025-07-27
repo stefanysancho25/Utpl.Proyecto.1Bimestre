42fbdbb1c08d4aaabe45bb9c29c393b52a8677ac
stefany <nicolebermudez156@hotmail.com>
2025-07-27 08:00:39 +0000

    seguridad

---
+++ api-inventario/src/main/java/com/tecnologiainventario/api_inventario/configuraciones/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.tecnologiainventario.api_inventario.configuraciones;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults()) // Basic Auth
+                .csrf(csrf -> csrf.disable()); // Swagger necesita que CSRF esté deshabilitado
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
